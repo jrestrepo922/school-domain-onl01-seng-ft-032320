7bc6960ba5dfda990c0b025a42cee3a11ed1f80b
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-31 16:43:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+class School
+  
+  
+ def initialize(name) 
+   @name = name 
+   @roster = {}
+ end 
+ 
+ def name=(name)
+   @name = name
+ end
+ 
+ def name 
+   @name
+ end 
+ 
+ def roster=(roster)
+  @roster = roster
+ end 
+
+ def roster
+   @roster 
+ end 
+ 
+
+ 
+ def add_student(student_name, grade)
+  
+  if(@roster.keys.include?(grade))
+  #we want this to run when no grade level currently exist 
+    @roster[grade] << student_name
+  else 
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end
+ end
+end
+
+
+
